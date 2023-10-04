f4a963420368a885788c70df33296ae1f76b89f5
Srijith Rajamohan <srrajamohan@NAC02DD1QTMD6M.local>
2023-10-04 17:51:34 -0400

    Added accounting files

---
+++ README.md
@@ -0,0 +1,1 @@
+Accounting terminology from https://www.nysscpa.org/professional-resources/accounting-terminology-guide#sthash.WK7I3nSn.dyimZQDy.dpbs
---
+++ write_accounting_json.py
@@ -0,0 +1,29 @@
+import json
+
+with open('accounting_clean.txt', 'r') as f:
+    buf = f.readlines()
+
+accounting_keys = [buf[idx] for idx in range(0,len(buf)) if idx%2 == 0]
+accounting_vals = [buf[idx] for idx in range(0,len(buf)) if idx%2 != 0]
+
+accounting_list = []
+for elem in zip(accounting_keys, accounting_vals):
+    accounting_list.append(elem)
+
+with open('accounting_data.json', 'w') as f:
+    f.write(json.dumps(accounting_list))
+
+with open('accounting_data.json', 'r') as f:
+    data = json.load(f)
+
+data_split = [[elem[0], elem[1].strip().split('.')] for elem in data]
+
+data_split_cleaned = []
+for elem in data_split:
+    if('' in elem[1]):
+        data_split_cleaned.append([elem[0].strip(), elem[1].remove('')])
+    else:
+        data_split_cleaned.append([elem[0].strip(), elem[1]])
+
+with open('accounting.json', 'w') as f:
+    f.write(json.dumps(data_split_cleaned))
